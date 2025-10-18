532e54eb78a8b0562e598f7ac19ef6fdfe5a3028
gco-12345 <gcomccray@gmail.com>
2025-10-17 18:42:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function check() {
+  var passcode = document.getElementById('input').value.trim();
+  if (passcode === "Alpha67") {
+    window.location.href = "https://gco-12345.github.io/vscloack/";
+  }
+  else {
+    alert("try again");
+  }
+}
